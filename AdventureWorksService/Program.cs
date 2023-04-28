0b73d9957a647d3703e3f88161a52f4cb8899d02
hvittorelli <misshopev@gmail.com>
2023-04-28 17:28:21 -0400

    Add project files.

---
+++ AdventureWorksService/Program.cs
@@ -0,0 +1,36 @@
+using AdventureWorksService.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<CustomerContext>(optionsBuilder => optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDB")));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c=> {c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdventureWorksService", Version = "v1" }); });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
+
+
